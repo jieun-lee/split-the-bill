aa5f42aa11bfbcc209dad051a197f66e9e16e136
Jieun Lee <jieun-lee@users.noreply.github.com>
2025-02-24 18:32:23 -0800

    Add SplitTheBill component

---
+++ src/pages/SplitTheBill.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SplitTheBill: React.FC = () => {
+	return (
+		<div>
+			Split the Bill
+		</div>
+	)
+}
+
+export default SplitTheBill;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import SplitTheBill from "./SplitTheBill";
 
 export default function Home() {
   return (
@@ -9,7 +10,7 @@ export default function Home() {
 			<link rel="icon" href="/favicon.ico" />
       	</Head>
 		<main>
-			Split the Bill App
+			<SplitTheBill />
 		</main>
     </>
   );
